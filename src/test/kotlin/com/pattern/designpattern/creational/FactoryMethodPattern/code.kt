1819032aac898803e05eb6bf2931c6567378ff4b
Deoksoo(Eden) <deoksoo.lee@smartfoodnet.com>
2022-01-19 11:32:17 +0900

    feat : Factory Method added

---
+++ src/test/kotlin/com/pattern/designpattern/creational/FactoryMethodPattern/code.kt
@@ -0,0 +1,34 @@
+package com.pattern.designpattern.creational.FactoryMethodPattern
+
+interface Transport {
+    fun deliver() : String
+}
+
+class Truck : Transport{
+    override fun deliver(): String = "Truck Transport"
+}
+
+class Ship : Transport{
+    override fun deliver(): String = "Ship Transport"
+}
+
+class TransportFactory{
+    companion object {
+        fun createTransportation(type: String) = when (type) {
+            "truck" -> Truck()
+            "ship" -> Ship()
+            else -> throw IllegalArgumentException("Unknown Transport")
+        }
+    }
+}
+
+class DeliveryService{
+    fun delivery(type : String){
+        val transport = TransportFactory.createTransportation(type)
+        println(transport.deliver())
+    }
+}
+
+fun main() {
+    DeliveryService().delivery("truck")
+}
